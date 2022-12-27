940222f2b1be98a1b3a5f86c12d5e224e1e75a0b
Kristaps Ritins <kristaps@serpamediagroup.com>
2022-12-28 00:03:30 +0200

    handle unauth users

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -4,12 +4,16 @@ import Products from "../views/Products.vue";
 import Login from "../views/Login.vue";
 import ForgotPassword from "../views/ForgotPassword.vue";
 import AppLayout from "../components/AppLayout.vue";
-
+// import store from "../store"
+// 3:04 - handle unauth users
 const routes = [
     {
         path: '/app',
         name: 'app',
         component: AppLayout,
+        meta: {
+            requiresAuth:true
+        },
         children: [
             {
                 path: 'dashboard',
@@ -39,6 +43,14 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+});
+
+router.beforeEach((to, from, next) => {
+    if(to.meta.requiresAuth && !store.state.user.token) {
+        next({name: 'login'})
+    } else {
+        next()
+    }
 });
 
 export default router;
